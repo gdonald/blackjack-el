12ec927458d2d3f278e04d3ad40c1df75618e21f
Greg Donald <gdonald@gmail.com>
2023-05-09 18:04:44 -0500

    Try using a script

---
+++ packages.el
@@ -0,0 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+(package-refresh-contents)
+
+(package-install 'buttercup)
+(package-install 'undercover)
